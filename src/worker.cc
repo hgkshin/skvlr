9577cb3228663c7b3781fe1f419b779c3e885565
Omar Diab <o.s.diab@gmail.com>
2015-05-22 13:59:17 -0700

    comment worker methods, add persist method/stub put impl

--- src/worker.cc
+++ src/worker.cc
@@ -13,6 +13,10 @@ Worker::~Worker()
     /* Empty */
 }
 
+/**
+ * Listen for incoming requests by busy waiting and inspecting the queue of
+ * requests for new PENDING requests to service.
+ */
 void Worker::listen()
 {
     while(true) {
@@ -22,7 +26,6 @@ void Worker::listen()
             switch(req.type) {
                 case Skvlr::GET:
                     handle_get(req);
-                    req.sema.notify();
                     break;
                 case Skvlr::PUT:
                     handle_put(req);
@@ -34,7 +37,12 @@ void Worker::listen()
 }
 
 /**
- * @param req GET request to handle
+ * Handle a get request. Retreives data corresponding to req.key from memory,
+ * since all data is assumed to fit in memory.  Synchronous, so this releases
+ * req's semaphore before exiting.  Success/failure of the request is stored in
+ * req.status.
+ *
+ * @param req PENDING GET request to handle
  */
 void Worker::handle_get(Skvlr::request &req)
 {
@@ -45,19 +53,49 @@ void Worker::handle_get(Skvlr::request &req)
     auto value = data.find(req.key);
     if (value == data.end()) {
         req.status = Skvlr::ERROR;
-        return;
+        goto release_sema;
     }
 
     req.value = value->second;
     req.status = Skvlr::SUCCESS;
+
+release_sema:
+    req.sema.notify();
 }
 
+/**
+ * Handle an incoming put request. Asynchronous, so it attempts to persist the
+ * key and stores the new value in the worker's internal map if persisting
+ * succeeds. Inspect req.status to determine if the persistence has completed
+ * successfully or not.
+ *
+ * @param req PENDING PUT request to handle
+ */
 void Worker::handle_put(Skvlr::request &req)
 {
     /* Empty */
-    // TODO: implement me
     assert(req.type == Skvlr::PUT);
     assert(req.status == Skvlr::PENDING);
 
-    req.status = Skvlr::ERROR;
+    // TODO: implement persisting the data, add to map if persistence succeeds
+    int success = persist(req.key, req.value);
+    if (success != 0) {
+      req.status = Skvlr::ERROR;
+      return;
+    }
+
+    data.insert(std::pair<int, int>(req.key, req.value));
+    req.status = Skvlr::SUCCESS;
+}
+
+/**
+ * Persist a key-value pair in this worker's disk.
+ *
+ * @return 0 on success, or a negative value on failure.
+ */
+int Worker::persist(const int key, const int value)
+{
+    // TODO: implement me!
+    return -1;
+
 }
