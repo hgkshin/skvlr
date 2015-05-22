dd2dfad0957c12d394143311a7bd4948901b1764
Omar Diab <o.s.diab@gmail.com>
2015-05-22 13:58:58 -0700

    homogenize includes style

--- src/worker.cc
+++ src/worker.cc
@@ -1,6 +1,6 @@
-#include "worker.h"
 #include<assert.h>
 
+#include "worker.h"
 
 Worker::Worker(const int fd, const int worker_id, std::map<int, int> data)
   : fd(fd), worker_id(worker_id), data(data)
