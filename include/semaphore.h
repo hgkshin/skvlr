071206b3daec02e7e884484474f466cd1948d3ad
Omar Diab <o.s.diab@gmail.com>
2015-05-22 13:00:43 -0700

    add semaphore implementation

---
+++ include/semaphore.h
@@ -0,0 +1,31 @@
+#include <mutex>
+#include <condition_variable>
+
+#pragma once
+
+class Semaphore {
+public:
+    Semaphore (int count = 0) : count(count) {}
+
+    inline void notify()
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        count++;
+        cv.notify_one();
+    }
+
+    inline void wait()
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+
+        while(count == 0) {
+            cv.wait(lock);
+        }
+        count--;
+    }
+
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+    int count;
+};
