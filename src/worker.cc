bc19c7ecedd7983b079106cbbc70595c8ac9a03a
Omar Diab <o.s.diab@gmail.com>
2015-05-22 13:02:04 -0700

    implement basic get, stub listen method

--- src/worker.cc
+++ src/worker.cc
@@ -1,4 +1,5 @@
 #include "worker.h"
+#include<assert.h>
 
 
 Worker::Worker(const int fd, const int worker_id, std::map<int, int> data)
@@ -12,13 +13,51 @@ Worker::~Worker()
     /* Empty */
 }
 
-
-void Worker::handle_get(Skvlr::request &req __attribute__((unused)))
+void Worker::listen()
 {
-    /* Empty */
+    while(true) {
+        /* TODO: If data exists handle it */
+        if (false) {
+            Skvlr::request req; // TODO: pull from queue
+            switch(req.type) {
+                case Skvlr::GET:
+                    handle_get(req);
+                    req.sema.notify();
+                    break;
+                case Skvlr::PUT:
+                    handle_put(req);
+                    break;
+                default: assert(false); // sanity check
+            }
+        }
+    }
 }
 
-void Worker::handle_put(Skvlr::request &req __attribute__((unused)))
+/**
+ * @param req GET request to handle
+ */
+void Worker::handle_get(Skvlr::request &req)
+{
+    // TODO: handle gets to other cores.
+    assert(req.type == Skvlr::GET);
+    assert(req.status == Skvlr::PENDING);
+
+    auto value = data.find(req.key);
+    if (value == data.end()) {
+        req.status = Skvlr::ERROR;
+        return;
+    }
+
+    req.value = value->second;
+    req.status = Skvlr::SUCCESS;
+}
+
+void Worker::handle_put(Skvlr::request &req)
 {
     /* Empty */
+    // TODO: implement me
+    assert(req.type == Skvlr::PUT);
+    assert(req.status == Skvlr::PENDING);
+
+    req.status = Skvlr::ERROR;
 }
